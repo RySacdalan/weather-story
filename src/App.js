7d34f2e5d31da254026bd7cd3928c3e73cca9b2e
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-06-28 22:02:40 +0800

    Install axios & use API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import MainBody from "./layout/MainBody";
 import MainHeader from "./layout/MainHeader";
+import axios from "axios";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [locationKey, setLocationKey] = useState("");
+
+  const searchWeather = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${locationKey}&appid=c1473becaeff1ec279c5251e93a54071`
+        )
+        .then((response) => console.log(response.data))
+        .then((response) => setWeatherData(response.data))
+        .catch((err) => console.log(err.message));
+    }
+  };
+
   return (
     <div className="App">
       <div className="main_app_content">
-        <MainHeader />
-        <MainBody />
+        <div className="header_container">
+          <div className="header">
+            <h1>WeatherStory</h1>
+            <div className="header_search">
+              <input
+                className="header_input"
+                type="text"
+                placeholder="Search"
+                value={locationKey}
+                onChange={(e) => setLocationKey(e.target.value)}
+                onKeyPress={searchWeather}
+              />
+              <button className="header_btn" type="submit">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <MainBody weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Header.css'
 
-const Header = (props) => {
+const Header = ({value, setLocationKey}) => {
   return(
     <div className="header_container">
       <div className='header'>
           <h1>WeatherStory</h1>
           <div className='header_search'>
-            <input className='header_input' type="text" placeholder='Search' />
-            <button className='header_btn'>Search</button>
+            <input className='header_input' type="text" placeholder='Search' value={value} onChange={(e) => setLocationKey(e.target.value) }/>
+            <button className='header_btn' type='submit'>Search</button>
           </div>
       </div>
     </div>
