49636ed5f7043c7388e240e3c50e7f527d819c95
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-06-28 21:06:41 +0800

    Add more styles inside weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,7 +8,7 @@ const Weather = (props) => {
         <div className="weather_primary_details">
             <div className='primary_wrapper'>
                 <div className='primary_content'>
-                    <h1 className='weather_temp'>16deg</h1>
+                    <h1 className='weather_temp'>16&deg;F</h1>
                     <p className='weather_description'>light rain</p>
                 </div>
                 <div className='weather_location'>
@@ -20,15 +20,19 @@ const Weather = (props) => {
         <div className="weather_secondary_details">
             <div className="first-section">
                 <h1>Main</h1>
-                <p>Temp: 300.12</p>
-                <p>Feels like: 200.25</p>
-                <p>Humidity: 91</p>
+                <div className='main-details'>
+                  <p>Temp: 300.12</p>
+                  <p>Feels like: 200.25</p>
+                  <p>Humidity: 91</p>
+                </div>
             </div>
             <div className="second-section">
                 <h1>Wind</h1>
-                <p>Speed: 2.68</p>
-                <p>Deg: 207</p>
-                <p>Gust: 8.99</p>
+                <div className="wind-details">
+                  <p>Speed: 2.68</p>
+                  <p>Deg: 207</p>
+                  <p>Gust: 8.99</p>
+                </div>
             </div>
         </div>
       </div>
