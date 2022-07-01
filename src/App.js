75d26e9be2bfd98d9d6e213b0ddc9bbc3f32c6e2
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-07-01 21:29:29 +0800

    Render weather data from api

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         {/* For header searchbar */}
         <div className="header_container">
           <div className="header">
-            <h1>WeatherStory in {weatherData.name}</h1>
+            <h1>WeatherStory</h1>
             <div className="header_search">
               <input
                 className="header_input"
@@ -52,32 +52,73 @@ function App() {
         <div className="weather_wrapper">
           <div className="weather_content">
             <div className="weather_primary_details">
-              <div className="primary_wrapper">
-                <div className="primary_content">
-                  <h1 className="weather_temp">16&deg;F</h1>
-                  <p className="weather_description">light rain</p>
+              {locationKey ? (
+                <div className="primary_wrapper">
+                  <div className="primary_content">
+                    {weatherData.main ? (
+                      <h1 className="weather_temp">
+                        {weatherData.main.temp}&deg;F
+                      </h1>
+                    ) : null}
+                    {weatherData.weather ? (
+                      <p className="weather_main">
+                        {weatherData.weather[0].main}
+                      </p>
+                    ) : null}
+                  </div>
+                  <div className="weather_location">
+                    <div>
+                      {weatherData.weather ? (
+                        <p className="weather_description">
+                          {weatherData.weather[0].description}.
+                        </p>
+                      ) : null}
+                    </div>
+                    <div className="city_and_time">
+                      <p className="weather_city">
+                        {weatherData.name ? `${weatherData.name},` : null}
+                        {weatherData.sys ? `${weatherData.sys.country}.` : null}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="weather_location">
-                  <p className="weather_city">Manila, PH.</p>
-                  <p className="weather_time">12:30PM</p>
-                </div>
-              </div>
+              ) : (
+                <div>Search data</div>
+              )}
             </div>
+
             <div className="weather_secondary_details">
               <div className="first-section">
                 <h1>Main</h1>
                 <div className="main-details">
-                  <p>Temp: 300.12</p>
-                  <p>Feels like: 200.25</p>
-                  <p>Humidity: 91</p>
+                  <p>
+                    FEELS LIKE:{" "}
+                    {weatherData.main ? weatherData.main.feels_like : null}
+                    &deg;F
+                  </p>
+                  <p>
+                    HUMIDITY:{" "}
+                    {weatherData.main ? `${weatherData.main.humidity}%` : null}
+                  </p>
+                  <p>
+                    PRESSURE:{" "}
+                    {weatherData.main
+                      ? `${weatherData.main.pressure}psf`
+                      : null}
+                  </p>
                 </div>
               </div>
               <div className="second-section">
                 <h1>Wind</h1>
                 <div className="wind-details">
-                  <p>Speed: 2.68</p>
-                  <p>Deg: 207</p>
-                  <p>Gust: 8.99</p>
+                  <p>
+                    SPEED:{" "}
+                    {weatherData.wind ? `${weatherData.wind.speed}MPH` : null}
+                  </p>
+                  <p>
+                    DEGREES: {weatherData.wind ? weatherData.wind.deg : null}
+                    &deg;F
+                  </p>
                 </div>
               </div>
             </div>
